28c6ea5723505ef116d68e2f621990e047f76386
Brandon <Blabrake1@gmail.com>
2021-02-15 00:15:14 -0500

    Soccer Ball and collectibles

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    float speed;
+    public float speed;
     Rigidbody RB;
     float distToGround;
     [SerializeField]
---
+++ Assets/Scripts/SoccerBall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerBall : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float pushPower = 5.0f;
+    Vector3 force;
+    Vector3 startpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = this.gameObject.transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Vector3 pushdir = new Vector3(col.GetComponent<Rigidbody>().velocity.x, 0,
+                col.GetComponent<Rigidbody>().velocity.x);
+
+            rb.velocity = pushdir * pushPower;
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Players")
+        {
+            force = Vector3.zero;
+        }
+        if (col.gameObject.name == "Out_Of_Bounds")
+        {
+            this.gameObject.transform.position = startpos;
+            rb.velocity = new Vector3(0f, 0f, 0f);
+        }
+    }
+}
