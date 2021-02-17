141120915699ad3fa010cd9890fdab601f20a762
Brandon <Blabrake1@gmail.com>
2021-02-16 19:41:32 -0500

    pause

---
+++ Assets/Scripts/Player Scripts/PlayerPause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPause : MonoBehaviour
+{
+    [SerializeField]
+    bool IsPaused = false;
+    float PauseTimer;
+    private GameObject Player;
+    GameObject gameManager;
+    void Awake()
+    {
+        gameManager = GameObject.Find("GameManager");
+        Player = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (!IsPaused && PauseTimer < 0)
+            {
+                IsPaused = true;
+                Paused();
+                PauseTimer = 1;
+            }
+            if (IsPaused && PauseTimer < 0)
+            {
+                IsPaused = false;
+                UnPaused();
+                PauseTimer = 1;
+            }
+        }
+
+        PauseTimer -= Time.deltaTime;
+    }
+    void Paused()
+    {
+        Player.GetComponent<PlayerMovement>().enabled = false;
+        gameManager.GetComponent<PlayerSwitcher>().enabled = false;
+        gameManager.GetComponent<GameManager>().unlockCursor();
+    }
+    void UnPaused()
+    {
+        Player.GetComponent<PlayerMovement>().enabled = true;
+        gameManager.GetComponent<PlayerSwitcher>().enabled = true;
+        gameManager.GetComponent<GameManager>().lockCursor();
+    }
+}
---
+++ Assets/Scripts/TelePorter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelePorter : MonoBehaviour
+{
+    [Tooltip("The TelePorter that this teleporter will send you to")]
+    [SerializeField]
+    GameObject OtherTelePorter;
+
+    [Tooltip("If this is tagged true on one of the teleporters, it'll send you to the teleporter tagged true but wont send you back")]
+    [SerializeField]
+    bool OneWay = false;
+
+    private float TeleporterTimer;
+
+    void Update()
+    {
+        TeleporterTimer -= Time.deltaTime;
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if (TeleporterTimer < 0 && !OneWay)
+            {
+                OtherTelePorter.GetComponent<TelePorter>().TeleporterTimer = 2;
+                col.transform.position = OtherTelePorter.transform.position;
+            }
+        }
+    }
+}
