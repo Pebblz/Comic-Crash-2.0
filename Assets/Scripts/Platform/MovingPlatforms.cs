1f8e0269d11e137c4ee8551a0d49dcc5e4a5e62c
Brandon <Blabrake1@gmail.com>
2021-02-20 21:11:45 -0500

    Improved Moving Platforms

--- Assets/Scripts/Platform/MovingPlatforms.cs
+++ Assets/Scripts/Platform/MovingPlatforms.cs
@@ -8,99 +8,184 @@ public class MovingPlatforms : MonoBehaviour
     [SerializeField]
     bool upAndDown;
 
-    [Tooltip("The amount the platform will move")]
+    [Tooltip("The amount the platform will move up and down")]
     [SerializeField]
-    float DistanceToMove;
+    float DistanceToMoveY;
+
+    [Tooltip("The amount the platform will move left nad right or striaght and back")]
+    [SerializeField]
+    float DistanceToMoveXZ;
 
     [Tooltip("If up and down if false, this'll will be for if the platform will move straight and back or left and right")]
     [SerializeField]
     bool LeftAndRight;
 
+    [Tooltip("If you want it to go up and down and what ever you put for left and right")]
+    [SerializeField]
+    bool Both;
+
     [Tooltip("The speed at which the platform moves")]
     [SerializeField]
     float speed = 2;
 
-    private float EndPoint;
+    [Tooltip("If you want it to activate when step on")]
+    [SerializeField]
+    bool StepOnToActivate;
+
+    private float EndPointY, EndPointX, EndPointZ;
 
     private Vector3 StartPoint;
 
-    private bool GoBack;
+    private bool GoBackY, GoBackX, GoBackZ, GoBackToStart;
+
+    private bool isSteppedOn;
     // Start is called before the first frame update
     void Start()
     {
         StartPoint = transform.position;
         if (upAndDown)
-            EndPoint = StartPoint.y + DistanceToMove;
+            EndPointY = StartPoint.y + DistanceToMoveY;
 
-        if (!upAndDown && !LeftAndRight)
-            EndPoint = StartPoint.z + DistanceToMove;
+        if (!LeftAndRight)
+            EndPointZ = StartPoint.z + DistanceToMoveXZ;
 
-        if (!upAndDown && LeftAndRight)
-            EndPoint = StartPoint.x + DistanceToMove;
+        if (LeftAndRight)
+            EndPointX = StartPoint.x + DistanceToMoveXZ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (upAndDown)
+        if (!StepOnToActivate && !GoBackToStart)
         {
-            if (GoBack)
+            Move();
+        }
+        if(StepOnToActivate && !GoBackToStart)
+        {
+            if(isSteppedOn)
             {
-                this.gameObject.transform.Translate(new Vector3(0, -1 * Time.deltaTime * speed, 0), Space.World);
-                if (this.gameObject.transform.position.y <= StartPoint.y)
-                {
-                    GoBack = false;
-                }
-            }
+                Move();
+            } 
             else
             {
-
-                this.gameObject.transform.Translate(new Vector3(0, 1 * Time.deltaTime * speed, 0), Space.World);
-                if (this.gameObject.transform.position.y >= EndPoint)
+                resetPosition();
+            }
+        }
+        if (GoBackToStart)
+        {
+            resetPosition();
+        }
+    }
+    private void Move()
+    {
+        if (upAndDown)
+        {
+            UpAndDown();
+            if (Both)
+            {
+                if (!LeftAndRight)
                 {
-                    GoBack = true;
+                    FrontToBack();
+                }
+                else
+                {
+                    LeftToRight();
                 }
             }
+
         }
         else
         {
             if (!LeftAndRight)
             {
-                if (GoBack)
-                {
-                    this.gameObject.transform.Translate(new Vector3(0, 0, -1 * Time.deltaTime * speed), Space.World);
-                    if (this.gameObject.transform.position.z <= StartPoint.z)
-                    {
-                        GoBack = false;
-                    }
-                } else
-                {
-                    this.gameObject.transform.Translate(new Vector3(0, 0, 1 * Time.deltaTime * speed), Space.World);
-                    if (this.gameObject.transform.position.z >= EndPoint)
-                    {
-                        GoBack = true;
-                    }
-                }
+                FrontToBack();
             }
             else
             {
-                if (GoBack)
-                {
-                    this.gameObject.transform.Translate(new Vector3(-1 * Time.deltaTime * speed, 0, 0), Space.World);
-                    if (this.gameObject.transform.position.x <= StartPoint.x)
-                    {
-                        GoBack = false;
-                    }
-                }
-                else
-                {
-                    this.gameObject.transform.Translate(new Vector3(1 * Time.deltaTime * speed, 0, 0), Space.World);
-                    if (this.gameObject.transform.position.x >= EndPoint)
-                    {
-                        GoBack = true;
-                    }
-                }
+                LeftToRight();
             }
+        }
+    }
+    void LeftToRight()
+    {
+        if (GoBackX)
+        {
+            this.gameObject.transform.Translate(new Vector3(-1 * Time.deltaTime * speed, 0, 0), Space.World);
+            if (this.gameObject.transform.position.x <= StartPoint.x)
+            {
+                GoBackX = false;
+            }
+        }
+        else
+        {
+            this.gameObject.transform.Translate(new Vector3(1 * Time.deltaTime * speed, 0, 0), Space.World);
+            if (this.gameObject.transform.position.x >= EndPointX)
+            {
+                GoBackX = true;
+            }
+        }
+    }
+    void FrontToBack()
+    {
+        if (GoBackZ)
+        {
+            this.gameObject.transform.Translate(new Vector3(0, 0, -1 * Time.deltaTime * speed), Space.World);
+            if (this.gameObject.transform.position.z <= StartPoint.z)
+            {
+                GoBackZ = false;
+            }
+        }
+        else
+        {
+            this.gameObject.transform.Translate(new Vector3(0, 0, 1 * Time.deltaTime * speed), Space.World);
+            if (this.gameObject.transform.position.z >= EndPointZ)
+            {
+                GoBackZ = true;
+            }
+        }
+    }
+    void UpAndDown()
+    {
+        if (GoBackY)
+        {
+            this.gameObject.transform.Translate(new Vector3(0, -1 * Time.deltaTime * speed, 0), Space.World);
+            if (this.gameObject.transform.position.y <= StartPoint.y)
+            {
+                GoBackY = false;
+            }
+        }
+        else
+        {
+
+            this.gameObject.transform.Translate(new Vector3(0, 1 * Time.deltaTime * speed, 0), Space.World);
+            if (this.gameObject.transform.position.y >= EndPointY)
+            {
+                GoBackY = true;
+            }
+        }
+    }
+    void resetPosition()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, StartPoint, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, StartPoint) < .1)
+        {
+            GoBackToStart = false;
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Player" && StepOnToActivate)
+        {
+            isSteppedOn = true;
+        }
+
+    }
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Player" && StepOnToActivate)
+        {
+            isSteppedOn = false;
         }
     }
 }
