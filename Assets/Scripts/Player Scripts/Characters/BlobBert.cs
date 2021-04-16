585ac231f1f13ed6ccf86771c1fdb04bbca4e420
Brandon <Blabrake1@gmail.com>
2021-04-16 15:14:33 -0400

    changed up blobert so he always falls through the grate

--- Assets/Scripts/Player Scripts/Characters/BlobBert.cs
+++ Assets/Scripts/Player Scripts/Characters/BlobBert.cs
@@ -4,30 +4,24 @@ using UnityEngine;
 
 public class BlobBert : MonoBehaviour
 {
-    [SerializeField,Tooltip("This is number the timer will get set to when you crouch. This is here so the player doesn't get stuck in the grate")] 
-    float CrouchTimer;
+    //[SerializeField,Tooltip("This is number the timer will get set to when you crouch. This is here so the player doesn't get stuck in the grate")] 
+    //float CrouchTimer;
 
-    private float currentTimer;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //private float currentTimer;
 
-    // Update is called once per frame
     void Update()
     {
-        //if you crouch you start your crouch timer;
-        if(Input.GetKeyDown(KeyCode.C))
-        {
-            currentTimer = CrouchTimer;
-        }
-        currentTimer -= Time.deltaTime;
+        ////if you crouch you start your crouch timer;
+        //if(Input.GetKeyDown(KeyCode.C))
+        //{
+        //    currentTimer = CrouchTimer;
+        //}
+        //currentTimer -= Time.deltaTime;
     }
     private void OnCollisionEnter(Collision col)
     {
         //if blobert crouches he can pass through the grates 
-        if (col.gameObject.tag == "Grate" && currentTimer > 0)
+        if (col.gameObject.tag == "Grate")
         {
             Physics.IgnoreCollision(this.gameObject.GetComponent<Collider>(), col.gameObject.GetComponent<Collider>());
         }
