7a050c4fc076861ea21cf6b25177d17cec4c0589
Brandon <Blabrake1@gmail.com>
2021-04-15 22:28:55 -0400

    when bloberts anims are done he can pass through grates

--- Assets/Scripts/Player Scripts/Characters/BlobBert.cs
+++ Assets/Scripts/Player Scripts/Characters/BlobBert.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BlobBert : MonoBehaviour
 {
+    [SerializeField,Tooltip("This is number the timer will get set to when you crouch. This is here so the player doesn't get stuck in the grate")] 
+    float CrouchTimer;
+
+    private float currentTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,20 @@ public class BlobBert : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if you crouch you start your crouch timer;
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            currentTimer = CrouchTimer;
+        }
+        currentTimer -= Time.deltaTime;
     }
+    private void OnCollisionEnter(Collision col)
+    {
+        //if blobert crouches he can pass through the grates 
+        if (col.gameObject.tag == "Grate" && currentTimer > 0)
+        {
+            Physics.IgnoreCollision(this.gameObject.GetComponent<Collider>(), col.gameObject.GetComponent<Collider>());
+        }
+    }
+    
 }
