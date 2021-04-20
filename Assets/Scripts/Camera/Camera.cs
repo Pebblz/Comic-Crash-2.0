9c4036e2a31f6bc3a442233489a911e50aa7f018
Brandon <Blabrake1@gmail.com>
2021-04-20 16:40:40 -0400

    Fixed camera bug

--- Assets/Scripts/Camera/Camera.cs
+++ Assets/Scripts/Camera/Camera.cs
@@ -134,7 +134,7 @@ public class Camera : MonoBehaviour
 
                 if (Physics.Linecast(target.position, transform.position, out hit))
                 {
-                    if (!hit.transform.gameObject.GetComponent<Player>())
+                    if (!hit.transform.gameObject.GetComponent<Player>() && hit.transform.gameObject.layer != 9)
                     {
 
                         //this if statements here so the camera doesn't bug out when colliding with a wall
