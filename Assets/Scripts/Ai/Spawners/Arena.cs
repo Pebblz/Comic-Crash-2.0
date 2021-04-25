90296f6da7945ca93c5ecb10ddfd4755f17edabd
Brandon <Blabrake1@gmail.com>
2021-04-24 23:13:04 -0400

    blobert + spawner

---
+++ Assets/Scripts/Ai/Spawners/Arena.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour
+{
+    [SerializeField] Spawner[] SpawnersInArena;
+    [SerializeField] bool SpawnersActiveOnStart;
+    public bool PlayerInArena;
+    GameObject Player;
+    List<GameObject> ColliderList;
+    [SerializeField] int NumberToSpawn;
+    void Start()
+    {
+        foreach (Spawner s in SpawnersInArena)
+        {
+            s.SpawnAmount = 0;
+            if(!SpawnersActiveOnStart)
+            {
+                s.Active = false;
+            }
+        }
+        Player = GameObject.FindGameObjectWithTag("Player");
+        for(int i = 0; i<NumberToSpawn;i=0)
+        {
+            
+            foreach(Spawner s in SpawnersInArena)
+            {
+                if (NumberToSpawn >= 0)
+                {
+                    s.SpawnAmount++;
+                    NumberToSpawn--;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        if(PlayerInArena)
+        {
+            foreach(Spawner s in SpawnersInArena)
+            {
+                s.Active = true;
+            }
+        }
+    }
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    if (!ColliderList.Contains(col.gameObject))
+    //    {
+    //        ColliderList.Add(col.gameObject);
+    //    }
+    //}
+    //void OnTriggerStay(Collider col)
+    //{
+
+    //}
+    //private void OnTriggerExit(Collider col)
+    //{
+    //    if (!ColliderList.Contains(col.gameObject))
+    //    {
+    //        ColliderList.Remove(col.gameObject);
+    //    }
+    //}
+}
--- Assets/Scripts/Ai/Spawners/Spawner.cs
+++ Assets/Scripts/Ai/Spawners/Spawner.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("If This is tied to an arena the arena will auto set this field")]
+    public int SpawnAmount;
+    private int spawned;
+    public bool Active;
+    private GameObject CurrentSpawnedEnemy;
+    public bool Beaten;
+    private void Start()
     {
-        
+        if (Active)
+        {
+            SpawnEnemy();
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (CurrentSpawnedEnemy == null && Active && spawned < SpawnAmount)
+        {
+            SpawnEnemy();
+            spawned++;
+        }
+        if(spawned == SpawnAmount)
+        {
+            Beaten = true;
+        }
+    }
+    void SpawnEnemy()
+    {
+
+    }
+    public void ResetSpawner()
+    {
+        spawned = 0;
+        Beaten = false;
+        Active = false;
     }
 }
