bbc2be0e4b071818f07902a3185ddc465f96c74b
Brandon <Blabrake1@gmail.com>
2021-04-26 18:47:09 -0400

    Did pats request
    Made a trigger to bring you back to the hub, and fixed the hub with everything new that was added in the past weeks

---
+++ Assets/Scripts/Objects In Game/EndOfLevelTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfLevelTrigger : MonoBehaviour
+{
+    LevelManager Lm;
+
+    [SerializeField, Tooltip("This is for the level index in the build settings. I have it to the hub at default")]
+    int LevelToLoad = 1;
+
+    void Start()
+    {
+        Lm = FindObjectOfType<LevelManager>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            Lm.LoadLevel(LevelToLoad);
+        }
+    }
+}
