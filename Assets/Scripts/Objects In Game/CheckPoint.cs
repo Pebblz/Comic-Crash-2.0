d42baae6c2d893d2c9b4b4ccda9636400a7f28e3
Brandon <Blabrake1@gmail.com>
2021-05-15 20:14:57 -0400

    checkpoints

--- Assets/Scripts/Objects In Game/CheckPoint.cs
+++ Assets/Scripts/Objects In Game/CheckPoint.cs
@@ -1,12 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(anim.GetCurrentAnimatorStateInfo(0).length > anim.GetCurrentAnimatorStateInfo(0).normalizedTime)
+        {
+            anim.SetBool("Spin", false);
+        }
+    }
     private void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player")
         {
-            col.GetComponent<Player>().respawnPoint = transform.position;
+            anim.SetBool("Spin", true);
+            col.GetComponent<Player>().respawnPoint = transform.position + new Vector3(0,2,0);
         }
     }
 }
