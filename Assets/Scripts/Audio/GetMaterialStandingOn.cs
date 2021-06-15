98f4a35ee1da62beba180fcbbc963368258b026a
Josh Cormier <jcorm714@gmail.com>
2021-06-15 16:06:03 -0400

    updated material detector

--- Assets/Scripts/Audio/GetMaterialStandingOn.cs
+++ Assets/Scripts/Audio/GetMaterialStandingOn.cs
@@ -14,38 +14,38 @@ public class GetMaterialStandingOn : MonoBehaviour
     [Range(0.5f, 1.5f)]
     [Tooltip("Distance to raycast for foot step detection")]
     float footStepCheckDistance = 0.5f;
-    
-    
+
+
     public Material current_mat;
     private GameObject player;
     private void Update()
-    {   
-        if(player == null)
+    {
+        if (player == null)
         {
             player = GameObject.FindGameObjectWithTag("Player");
 
         }
-        if (player.GetComponent<PlayerMovement>().OnGround)
+
+        RaycastHit hit;
+        if (Physics.Raycast(player.transform.position,
+                           player.transform.TransformDirection(Vector3.down),
+                           out hit,
+                           footStepCheckDistance))
         {
-            RaycastHit hit;
-            if(Physics.Raycast(player.transform.position, 
-                               player.transform.TransformDirection(Vector3.down),
-                               out hit,
-                               footStepCheckDistance)){
 
-                if (amIPissing)
-                {
-                    Debug.DrawRay(player.transform.position + new Vector3(0, 2f, 0),
-                                  player.transform.TransformDirection(Vector3.down) * hit.distance * 10,
-                                  Color.yellow, 3f);
-                }
-
-                Material mat = hit.collider.gameObject.GetComponent<Renderer>().material;
-                current_mat = mat;
-                
-
-            
+            if (amIPissing)
+            {
+                Debug.DrawRay(player.transform.position + new Vector3(0, 2f, 0),
+                              player.transform.TransformDirection(Vector3.down) * hit.distance * 10,
+                              Color.yellow, 3f);
             }
+
+            Material mat = hit.collider.gameObject.GetComponent<Renderer>().material;
+            current_mat = mat;
+
+
+
         }
     }
+
 }
