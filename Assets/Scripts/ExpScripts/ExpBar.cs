cbb80e6a565f2f1b78f63af76b9eeea5c402c647
Brandon <Blabrake1@gmail.com>
2021-06-15 14:36:59 -0400

    exp tree

---
+++ Assets/Scripts/ExpScripts/ExpBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpBar : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] CanvasGroup cg;
+    float fadeTimer = 3;
+
+    void Update()
+    {
+        fadeTimer -= Time.fixedDeltaTime;
+        if(fadeTimer < 0)
+        {
+            cg.alpha -= Time.fixedDeltaTime;
+        }
+        if (cg.alpha < 0)
+        {
+            slider.gameObject.SetActive(false);
+        }
+    }
+
+    public void newMax(int maxValue, int overFillValue) {
+        slider.maxValue = maxValue;
+        slider.value = overFillValue;
+    }
+    public void UpdateValue(int newValue) {
+        slider.value = newValue;
+        slider.gameObject.SetActive(true);
+        fadeTimer = 3;
+        cg.alpha = 1;
+    }
+}
