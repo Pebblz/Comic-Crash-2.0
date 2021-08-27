d1d468e1cf4d232c50889c4c575ab0f50fc70eb7
Brandon <Blabrake1@gmail.com>
2021-08-27 16:46:49 -0400

    Day 3 of no pat things

---
+++ Assets/Scripts/Objects In Game/CharacterSwitcherMachine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitcherMachine : MonoBehaviour
+{
+    [SerializeField]
+    GameObject CharacterSwitcherUI;
+
+    GameObject Player;
+
+    [SerializeField, Range(.1f, 20f)]
+    float Range;
+    private void Update()
+    {
+        if (Player == null)
+            Player = FindObjectOfType<PlayerMovement>().gameObject;
+
+        if(InRadius())
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                CharacterSwitcherUI.SetActive(!CharacterSwitcherUI.activeSelf);
+        }
+
+    }
+
+    bool InRadius()
+    {
+        if(Vector3.Distance(gameObject.transform.position, Player.transform.position) < Range)
+        {
+            return true;
+        }
+
+
+        return false;
+    }
+}
