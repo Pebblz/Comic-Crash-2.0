700c46b89fe758ef024c0e7d0f5095cc0e10fb53
Brandon <Blabrake1@gmail.com>
2021-09-20 23:58:14 -0400

    Jelly fish circle mines

--- Assets/Scripts/Ai/JellyFish.cs
+++ Assets/Scripts/Ai/JellyFish.cs
@@ -7,17 +7,20 @@ public class JellyFish : MonoBehaviour
     [SerializeField, Range(0f, 40f), Tooltip("The amount the JellyFish will move up and down")]
     float DistanceToMoveY;
 
-    [SerializeField, Range(1f, 10f), Tooltip("The speed at which the JellyFish moves")]
+    [SerializeField, Range(0f, 10f), Tooltip("The speed at which the JellyFish moves")]
     float speed = 2;
 
     [SerializeField, Range(1f, 10f), Tooltip("The amount of damage done to the player")]
     float damage = 2;
-
+    [SerializeField]
+    float TimeToWait;
     private bool GoBackY;
 
     private Vector3 StartPoint;
 
     private float EndPointY;
+
+    private float Timer;
     private void Start()
     {
         StartPoint = transform.position;
@@ -27,24 +30,31 @@ public class JellyFish : MonoBehaviour
     private void Update()
     {
         UpAndDown();
+        if(Timer > 0)
+            Timer -= Time.deltaTime;
     }
     void UpAndDown()
     {
-        if (GoBackY)
+        if (Timer <= 0)
         {
-            this.gameObject.transform.Translate(new Vector3(0, -1 * Time.deltaTime * speed, 0), Space.World);
-            if (this.gameObject.transform.position.y <= StartPoint.y)
+            if (GoBackY)
             {
-                GoBackY = false;
+                this.gameObject.transform.Translate(new Vector3(0, -1 * Time.deltaTime * speed, 0), Space.World);
+                if (this.gameObject.transform.position.y <= StartPoint.y)
+                {
+                    GoBackY = false;
+                    Timer = TimeToWait;
+                }
             }
-        }
-        else
-        {
-
-            this.gameObject.transform.Translate(new Vector3(0, 1 * Time.deltaTime * speed, 0), Space.World);
-            if (this.gameObject.transform.position.y >= EndPointY)
+            else
             {
-                GoBackY = true;
+
+                this.gameObject.transform.Translate(new Vector3(0, 1 * Time.deltaTime * speed, 0), Space.World);
+                if (this.gameObject.transform.position.y >= EndPointY)
+                {
+                    GoBackY = true;
+                    Timer = TimeToWait;
+                }
             }
         }
     }
---
+++ Assets/Scripts/Ai/JellyFishRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyFishRotation : MonoBehaviour
+{
+    [SerializeField] float speed;
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+        //Quaternion q = Quaternion.FromToRotation(transform.GetChild(0).up, Vector3.up) * transform.GetChild(0).rotation;
+        //transform.GetChild(0).rotation = Quaternion.Slerp(transform.GetChild(0).rotation, q, Time.deltaTime * speed);
+    }
+}
