d195d5f45d02ce7b76663ca5c7a8f0c7d7c3ce88
Brandon <Blabrake1@gmail.com>
2021-12-08 16:28:44 -0500

    push player away script

---
+++ Assets/Scripts/Objects In Game/PushBack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBack : MonoBehaviour
+{
+    [SerializeField] float pushHeight;
+    [SerializeField] float pushDistince;
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 _velocity = collision.contacts[0].point - collision.gameObject.transform.position;
+
+            _velocity = -_velocity.normalized;
+
+            _velocity.y = pushHeight;
+
+            collision.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(_velocity.x * pushDistince,
+                _velocity.y, _velocity.z * pushDistince);
+
+            //faces direction on jump
+            collision.gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(_velocity.x * pushDistince,
+                _velocity.y, _velocity.z *  pushDistince));
+
+            //collision.gameObject.GetComponent<PlayerMovement>().PreventSnapToGround();
+        }
+
+    }
+}
