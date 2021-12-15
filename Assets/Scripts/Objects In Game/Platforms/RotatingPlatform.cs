3cd0cee71d8634127ed8e534bd8924f7e696ac18
Brandon <Blabrake1@gmail.com>
2021-12-15 17:42:36 -0500

    Update RotatingPlatform.cs

--- Assets/Scripts/Objects In Game/Platforms/RotatingPlatform.cs
+++ Assets/Scripts/Objects In Game/Platforms/RotatingPlatform.cs
@@ -17,5 +17,9 @@ public class RotatingPlatform : MonoBehaviour
         if (active)
             transform.Rotate(direction, RotationSpeed * Time.deltaTime);
     }
-
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+            collision.collider.transform.RotateAround(transform.position, direction, RotationSpeed * Time.deltaTime);
+    }
 }
