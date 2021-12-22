80c035c2b08843d6348fdd7af6ee1d1be93bb9a0
Brandon <Blabrake1@gmail.com>
2021-12-22 16:07:03 -0500

    The start of the main menu

---
+++ Assets/Scripts/Photon/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField] GameObject SinglePlayerBTN;
+    [SerializeField] GameObject MultiPlayerBTN;
+    private void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+    public void SinglePlayer()
+    {
+        RoomOptions roomOps = new RoomOptions() { IsVisible = false, IsOpen = false, MaxPlayers = 1 };
+        string s = "SinglePlayer " + Random.Range(0, 10000);
+        PhotonNetwork.CreateRoom(s, roomOps);
+        PhotonNetwork.JoinRoom(s);
+        
+    }
+    public override void OnJoinedLobby()
+    {
+        SinglePlayerBTN.SetActive(true);
+        MultiPlayerBTN.SetActive(true);
+
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("void 2");
+
+    }
+    public void Multiplayer()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+
+
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            PhotonNetwork.LoadLevel(3);
+        }
+    }
+}
