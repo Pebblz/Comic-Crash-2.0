04f77328f74552eea9e8463ffe52c5eb9422cc1e
Brandon <Blabrake1@gmail.com>
2021-12-21 20:05:12 -0500

    Underwater pokey that hurts you

---
+++ Assets/Scripts/UnderWater/UnderWaterPokey.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderWaterPokey : MonoBehaviour
+{
+    [SerializeField] float pushHeight;
+    [SerializeField] float pushDistince;
+    [SerializeField] int DamageToPlayer = 1;
+    private void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            col.collider.gameObject.GetComponent<PlayerHealth>().HurtPlayer(DamageToPlayer);
+
+            Vector3 _velocity = col.contacts[0].point - col.gameObject.transform.position;
+
+            _velocity = -_velocity.normalized;
+
+            _velocity.y = pushHeight;
+
+            col.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(_velocity.x * pushDistince,
+                _velocity.y, _velocity.z * pushDistince);
+
+            //faces direction on jump
+            col.gameObject.transform.rotation = Quaternion.LookRotation(new Vector3(_velocity.x * pushDistince,
+                _velocity.y, _velocity.z * pushDistince));
+
+        }
+
+    }
+}
