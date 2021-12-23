9dd51e7ac7d126a75137be190d51df462422bd92
Brandon <Blabrake1@gmail.com>
2021-12-22 22:29:39 -0500

    Added layer and script for 3D skybox

---
+++ Assets/Scripts/Camera/SkyBoxCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxCamera : MonoBehaviour
+{
+    Transform playercam;
+    [SerializeField] private float skyboxScale;
+
+    private void Start()
+    {
+        playercam = FindObjectOfType<MainCamera>().transform;
+    }
+    void Update()
+    {
+        transform.rotation = playercam.rotation;
+        transform.localPosition = playercam.position / skyboxScale;
+    }
+}
--- Assets/Scripts/Photon/MainMenu.cs
+++ Assets/Scripts/Photon/MainMenu.cs
@@ -22,25 +22,21 @@ public class MainMenu : MonoBehaviourPunCallbacks
         string s = "SinglePlayer " + Random.Range(0, 10000);
         PhotonNetwork.CreateRoom(s, roomOps);
         PhotonNetwork.JoinRoom(s);
-        
+        //PhotonNetwork.OfflineMode = true;
     }
     public override void OnJoinedLobby()
     {
         SinglePlayerBTN.SetActive(true);
         MultiPlayerBTN.SetActive(true);
-
     }
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel("void 2");
-
     }
     public void Multiplayer()
     {
         if (PhotonNetwork.IsMasterClient)
         {
-
-
             PhotonNetwork.CurrentRoom.IsOpen = false;
             PhotonNetwork.LoadLevel(3);
         }
