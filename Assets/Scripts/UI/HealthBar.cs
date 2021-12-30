449e4279c0961d7e05d2a7d35834f8dc1f078ef9
Brandon <Blabrake1@gmail.com>
2021-12-29 23:30:21 -0500

    HealthBar works

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] GameObject[] HealthBars = new GameObject[4];
+    GameObject player;
+    private void Update()
+    {
+        if (player == null)
+            player = PhotonFindCurrentClient();
+    }
+
+    public void FindNewHealthBar()
+    {
+        if (player.GetComponent<PlayerHealth>().currentHealth > 0)
+        {
+            HealthBars[player.GetComponent<PlayerHealth>().currentHealth].SetActive(false);
+            HealthBars[player.GetComponent<PlayerHealth>().currentHealth - 1].SetActive(true);
+        }
+        else
+        {
+            HealthBars[player.GetComponent<PlayerHealth>().currentHealth].SetActive(false);
+            HealthBars[3].SetActive(true);
+        }
+    }
+    GameObject PhotonFindCurrentClient()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject g in players)
+        {
+            if (g.GetComponent<PhotonView>().IsMine)
+                return g;
+        }
+        return null;
+    }
+}
