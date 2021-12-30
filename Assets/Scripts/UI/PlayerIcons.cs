46bc265d67653175a710a66e90bd981b2606f487
Brandon <Blabrake1@gmail.com>
2021-12-30 13:09:04 -0500

    Forgot to push tag manager

--- Assets/Scripts/UI/PlayerIcons.cs
+++ Assets/Scripts/UI/PlayerIcons.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerIcons : MonoBehaviour
 {
     [SerializeField] GameObject[] Icons = new GameObject[3];
-
     [SerializeField] Transform Main;
     Vector3 MainScale;
     Vector3 SmallScale;
@@ -13,6 +12,7 @@ public class PlayerIcons : MonoBehaviour
     [SerializeField] Transform[] iconPositions = new Transform[3];
     [SerializeField] Transform[] iconCurrentPositions = new Transform[3];
 
+    int MiddleCurrentIndex;
     private void Start()
     {
         MainScale = Main.localScale;
@@ -20,45 +20,53 @@ public class PlayerIcons : MonoBehaviour
     }
     public void MoveLeft()
     {
-        Transform[] temp = iconCurrentPositions;
-        for (int i = 0; i < 1; i++)
-        {
+        //Transform[] temp = iconCurrentPositions;
+        //if(MiddleCurrentIndex == 0)
+        //{
+        //    MiddleCurrentIndex = 2;
+        //}
+        //else
+        //    MiddleCurrentIndex--;
 
-            iconCurrentPositions[0] = temp[2];
+        
+        //iconCurrentPositions[0] = temp[2];
 
-            iconCurrentPositions[1] = temp[0];
+        //iconCurrentPositions[1] = temp[0];
 
-            iconCurrentPositions[2] = temp[1];
+        //iconCurrentPositions[2] = temp[1];
 
-            Icons[2].transform.position = iconPositions[1].position;
-            Icons[2].transform.localScale = SmallScale;
+        //Icons[2].transform.position = iconPositions[1].position;
+        //Icons[2].transform.localScale = SmallScale;
 
-            Icons[1].transform.position = iconPositions[0].position;
-            Icons[1].transform.localScale = MainScale;
+        //Icons[1].transform.position = iconPositions[0].position;
+        //Icons[1].transform.localScale = MainScale;
 
 
-            Icons[0].transform.position = iconPositions[2].position;
-            Icons[0].transform.localScale = SmallScale;
-        }
+        //Icons[0].transform.position = iconPositions[2].position;
+        //Icons[0].transform.localScale = SmallScale;
+
     }
     public void MoveRight()
     {
-        Transform[] temp = iconCurrentPositions;
-        for (int i = 0; i < 1; i++)
-        {
+        //Transform[] temp = iconCurrentPositions;
+        //if(MiddleCurrentIndex == 2)
+        //{
+        //    MiddleCurrentIndex = 0;
+        //}
+        //else
+        //    MiddleCurrentIndex++;
+        //iconCurrentPositions[2] = temp[0];
+        //iconCurrentPositions[1] = temp[2];
+        //iconCurrentPositions[0] = temp[1];
 
-            iconCurrentPositions[2] = temp[0];
-            iconCurrentPositions[1] = temp[2];
-            iconCurrentPositions[0] = temp[1];
+        //Icons[0].transform.position = iconPositions[1].position;
+        //Icons[0].transform.localScale = MainScale;
 
-            Icons[0].transform.position = iconPositions[1].position;
-            Icons[0].transform.localScale = MainScale;
+        //Icons[2].transform.position = iconPositions[0].position;
+        //Icons[2].transform.localScale = SmallScale;
 
-            Icons[2].transform.position = iconPositions[0].position;
-            Icons[2].transform.localScale = SmallScale;
+        //Icons[1].transform.position = iconPositions[2].position;
+        //Icons[1].transform.localScale = MainScale;
 
-            Icons[1].transform.position = iconPositions[2].position;
-            Icons[1].transform.localScale = MainScale;
-        }
     }
 }
