8de47990419a4e71f6885ba0dbfa31996941c65e
Brandon <Blabrake1@gmail.com>
2022-01-01 00:45:55 -0500

    Pat trigger

---
+++ Assets/Scripts/Objects In Game/Triggers/PatTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+public class PatTrigger : MonoBehaviour
+{
+    [SerializeField] int LevelIndex;
+
+
+    private void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+            PhotonNetwork.LoadLevel(LevelIndex);
+    }
+}
