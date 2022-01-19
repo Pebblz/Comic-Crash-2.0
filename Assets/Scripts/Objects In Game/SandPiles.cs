131383283ce674b156394a0f69f653085d6292e5
Brandon <Blabrake1@gmail.com>
2022-01-19 15:16:36 -0500

    Update SandPiles.cs

--- Assets/Scripts/Objects In Game/SandPiles.cs
+++ Assets/Scripts/Objects In Game/SandPiles.cs
@@ -7,20 +7,45 @@ public class SandPiles : MonoBehaviour
 {
     [SerializeField]
     GameObject Coin;
-
+    [SerializeField, Range(.1f, 6f)]
+    float HowFastToSquish = 3f;
+    [SerializeField]
+    float TimerToDelete = .2f;
     PhotonView photonView;
-
-    public void DestroyPile()
+    bool squish ;
+    private void Start()
     {
         photonView = GetComponent<PhotonView>();
-        photonView.RPC("DestroyBox", RpcTarget.All);
+    }
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            if (squish)
+            {
+                Squish();
+                if (TimerToDelete <= 0)
+                    photonView.RPC("DestroyBox", RpcTarget.All);
+
+                TimerToDelete -= Time.deltaTime;
+            }
+        }
+    }
+    public void DestroyPile()
+    {
+
+        squish = true;
     }
     [PunRPC]
     void DestroyBox()
     {
-        if(Coin != null)
-            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", Coin.name), transform.position + new Vector3(0,.5f,0), transform.rotation);
-        
+        if (Coin != null)
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", Coin.name), transform.position + new Vector3(0, .5f, 0), transform.rotation);
+
         PhotonNetwork.Destroy(photonView);
+    }
+    void Squish()
+    {
+        transform.localScale -= new Vector3(0, HowFastToSquish, 0) * Time.deltaTime;
     }
 }
